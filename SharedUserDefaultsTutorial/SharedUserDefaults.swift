e73ca20fcac2e0f99f4c365d4bd1849bd35956db
Alex <therebeloper@gmail.com>
2018-12-19 19:03:27 +0200

    first commit

---
+++ SharedUserDefaultsTutorial/SharedUserDefaults.swift
@@ -0,0 +1,17 @@
+//
+//  SharedUserDefaults.swift
+//  SharedUserDefaultsTutorial
+//
+//  Created by Alex Nagy on 19/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import Foundation
+
+struct SharedUserDefaults {
+    static let suiteName = "group.com.Rebeloper.SharedUserDefaultsTutorial"
+    
+    struct Keys {
+        static let username = "username"
+    }
+}
--- SharedUserDefaultsTutorial/ViewController.swift
+++ SharedUserDefaultsTutorial/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+let sharedUserDefaults = UserDefaults(suiteName: SharedUserDefaults.suiteName)
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let username = "Alex"
+        
+//        UserDefaults.standard.set(username, forKey: SharedUserDefaults.Keys.username)
+        
+//        guard let savedUsername = UserDefaults.standard.string(forKey: SharedUserDefaults.Keys.username) else { return }
+//        print("Username: \(savedUsername)")
+        
+        sharedUserDefaults?.set(username, forKey: SharedUserDefaults.Keys.username)
+        guard let sharedUsername = sharedUserDefaults?.string(forKey: SharedUserDefaults.Keys.username) else { return }
+        print("Shared username: \(sharedUsername)")
     }
 
 
